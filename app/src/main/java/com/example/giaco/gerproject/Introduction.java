cd16e37763dd897ef85643ed67ef7cc042625c60
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-02-10 18:06:08 +0100

    Introduction bozza -> login bozza

--- app/src/main/java/com/example/giaco/gerproject/Introduction.java
+++ app/src/main/java/com/example/giaco/gerproject/Introduction.java
@@ -1,13 +1,31 @@
 package com.example.giaco.gerproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Introduction extends AppCompatActivity {
+
+    Button hocapito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduction_page);
+
+
+        hocapito = (Button) findViewById(R.id.hocapito);
+
+        hocapito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginPage = new Intent(Introduction.this,
+                        LoginPage.class);
+                startActivity(LoginPage);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/giaco/gerproject/LoginPage.java
+++ app/src/main/java/com/example/giaco/gerproject/LoginPage.java
@@ -1,4 +1,13 @@
 package com.example.giaco.gerproject;
 
-public class LoginPage {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
 }
