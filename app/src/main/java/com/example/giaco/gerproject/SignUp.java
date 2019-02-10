c13e29e9f22badce36d1b89f5346e740ff03800f
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-02-10 18:34:19 +0100

    Introduction bozza -> login bozza

---
+++ app/src/main/java/com/example/giaco/gerproject/SignUp.java
@@ -0,0 +1,13 @@
+package com.example.giaco.gerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
