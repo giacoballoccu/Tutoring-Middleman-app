13cfd5fc3240bd13ac7a129090315830ed705577
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-02-10 16:33:26 +0100

    First Commit

---
+++ app/src/main/java/com/example/giaco/gerproject/Introduction.java
@@ -0,0 +1,13 @@
+package com.example.giaco.gerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_page);
+    }
+}
