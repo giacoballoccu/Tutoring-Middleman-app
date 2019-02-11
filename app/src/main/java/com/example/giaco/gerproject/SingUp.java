8f17a3175aaade12377303090a240f3527709077
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-02-11 16:28:26 +0100

    r problem

--- app/src/main/java/com/example/giaco/gerproject/SingUp.java
+++ app/src/main/java/com/example/giaco/gerproject/SingUp.java
@@ -1,5 +1,5 @@
 package com.example.giaco.gerproject;
-
+import com.example.giaco.gerproject.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -7,17 +7,19 @@ import android.widget.EditText;
 public class SingUp extends AppCompatActivity {
 
     EditText email, nome, cognome, password, confermaPassword, cellulare;
+    UserFactory factory = UserFactory.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sing_up);
 
-        email = (EditText) FindViewById(R.id.emailField);
-        nome = (EditText) FindViewById(R.id.nameField);
-        cognome = (EditText) FindViewById(R.id.surnameField);
-        password = (EditText) FindViewById(R.id.passwordField);
-        confermaPassword = (EditText) FindViewById(R.id.confirmPasswordField);
-        cellulare = (EditText) FindViewById(R.id.phoneField);
+        email = (EditText) findViewById(R.id.emailField);
+        nome = (EditText) findViewById(R.id.nameField);
+        cognome = (EditText) findViewById(R.id.surnameField);
+        password = (EditText) findViewById(R.id.passwordField);
+        confermaPassword = (EditText) findViewById(R.id.confirmPasswordField);
+        cellulare = (EditText) findViewById(R.id.phoneField);
+        
     }
 }
