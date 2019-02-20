211ef779b0100398b9334ecf09c877a895c289ff
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-02-20 23:06:10 +0100

    PACKAGE AGGIUNTI

---
+++ app/src/main/java/com/example/giaco/gerproject/Package.java
@@ -0,0 +1,43 @@
+package com.example.giaco.gerproject;
+
+import android.graphics.drawable.Drawable;
+
+public class Package {
+    private Drawable image;
+    private int nOre;
+    private float prezzo;
+
+    public Package(){
+
+    }
+
+    public Package(Drawable image, int nOre, float prezzo){
+        this.setImage(image);
+        this.setnOre(nOre);
+        this.setPrezzo(prezzo);
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public int getnOre() {
+        return nOre;
+    }
+
+    public void setnOre(int nOre) {
+        this.nOre = nOre;
+    }
+}
---
+++ app/src/main/java/com/example/giaco/gerproject/PackageFactory.java
@@ -0,0 +1,45 @@
+package com.example.giaco.gerproject;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
+
+import java.util.ArrayList;
+
+public class PackageFactory {
+    private static PackageFactory singleton;
+
+    private ArrayList<Package> packages = new ArrayList<>();
+
+    public static PackageFactory getInstance() {
+        if (singleton == null) {
+            singleton = new PackageFactory();
+        }
+        return singleton;
+    }
+
+    public PackageFactory(){
+        Context context = ApplicationContextProvider.getContext();
+
+        Package pack1 = new Package();
+        Drawable img1 = ResourcesCompat.getDrawable(context.getResources(), R.drawable.libroarrabbiato, null);
+        pack1.setImage(img1);
+
+        getPackages().add(pack1);
+
+        Package pack2 = new Package();
+
+        Drawable img2 = ResourcesCompat.getDrawable(context.getResources(), R.drawable.libroprincipe, null);
+        pack1.setImage(img2);
+
+        getPackages().add(pack2);
+    }
+
+    public ArrayList<Package> getPackages() {
+        return packages;
+    }
+
+    public void setPackages(ArrayList<Package> packages) {
+        this.packages = packages;
+    }
+}
