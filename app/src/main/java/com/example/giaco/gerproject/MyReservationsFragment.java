34822a113a15adb4c5e710a6ff9c1b138f1999d7
Rick <rick.espa@gmail.com>
2019-02-21 10:51:01 +0100

    prenotazioni statico finito, da fare dinamico

--- app/src/main/java/com/example/giaco/gerproject/MyReservationsFragment.java
+++ app/src/main/java/com/example/giaco/gerproject/MyReservationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.giaco.gerproject;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,8 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.giaco.gerproject.Classes.Reservation;
+import com.example.giaco.gerproject.Classes.ReservationFactory;
+
+import java.util.ArrayList;
 
 public class MyReservationsFragment extends Fragment {
+    LinearLayout mparent;
+    LayoutInflater layoutInflater;
+    View myView;
+
+    ReservationFactory factory = ReservationFactory.getInstance();
+    ArrayList<Reservation> resList = factory.getReservations();
 
     @Nullable
     @Override
@@ -20,5 +33,15 @@ public class MyReservationsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Prenotazioni");
+
+        mparent = view.findViewById(R.id.parentview_reservation);
+        layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        myView = layoutInflater.inflate(R.layout.reservation_static, null, false);
+        mparent.addView(myView);
+
+        myView = layoutInflater.inflate(R.layout.single_reservation, null, false);
+        mparent.addView(myView);
+
     }
 }
