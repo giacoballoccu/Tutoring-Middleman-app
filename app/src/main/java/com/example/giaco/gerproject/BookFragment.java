7180bc51b6491ef8e89c318446aa06e7290df616
Rick <rick.espa@gmail.com>
2019-02-21 16:49:06 +0100

    post-it forse sono cliccabili

---
+++ app/src/main/java/com/example/giaco/gerproject/BookFragment.java
@@ -0,0 +1,39 @@
+package com.example.giaco.gerproject;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.giaco.gerproject.Classes.Reservation;
+import com.example.giaco.gerproject.Classes.ReservationFactory;
+
+import java.util.ArrayList;
+
+public class BookFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.prenotaripetizione, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Prenota una ripetizione");
+
+
+    }
+
+
+}
