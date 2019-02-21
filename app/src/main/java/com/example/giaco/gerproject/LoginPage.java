55a874a689c45c6101053c8d41d9c3a88542ece4
Rick <rick.espa@gmail.com>
2019-02-21 11:51:42 +0100

    commentato le immagini dentro UserStudenteFactory, cambiata la grafica in verde e nero ma è da rivedere il colore.

--- app/src/main/java/com/example/giaco/gerproject/LoginPage.java
+++ app/src/main/java/com/example/giaco/gerproject/LoginPage.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.giaco.gerproject.Classes.UserStudente;
 import com.example.giaco.gerproject.Classes.UserStudenteFactory;
