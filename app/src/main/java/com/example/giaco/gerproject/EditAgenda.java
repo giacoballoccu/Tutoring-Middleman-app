ed5648cfd65e133311f15357201b3297b539aea8
EnricoCarnio <enricocarnio@hotmail.it>
2019-02-24 19:24:34 +0100

    agenda ---

--- app/src/main/java/com/example/giaco/gerproject/EditAgenda.java
+++ app/src/main/java/com/example/giaco/gerproject/EditAgenda.java
@@ -1,6 +1,5 @@
 package com.example.giaco.gerproject;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,82 +11,56 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.TextView;
-import com.example.giaco.gerproject.Classes.DisponibilitaFactory;
 
-
-public class EditAgenda extends Fragment {
+public class EditAgenda extends Fragment{
     private int anno = 100;
     private int mese = 100;
     private int giorno = 100;
-    CalendarView calendario;
-    String emailLoggedUser;
-    PersonalPageFragment p;
-    LayoutInflater layoutInflater;
     View myView;
-    Button butt;
+    CalendarView calendario;
+    Button conferma;
+    PersonalPageFragment profilo;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_editagenda, container, false);
+        //if (loggedUser.getEmail().equals(factory.getMittente()))
+        return inflater.inflate(R.layout.conversazioni_fragment, container, false);
     }
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("Aggiungi una data");
-        if (getArguments() != null) {
-            emailLoggedUser = getArguments().getString("actualUserMail");
-        }
-        //aparent = (LinearLayout) view.findViewById(R.id.parentview);
-        //layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        conferma = myView.findViewById(R.id.editAgendaButton);
 
-        //staticView = layoutInflater.inflate(R.layout.ore_rimanenti_static, null, false);
-        //mparent.addView(staticView);
-        ///aux = (TextView) staticView.findViewById(R.id.n_ore_rimanenti);
-        //ore_rimaste = Integer.parseInt(aux.getText().toString());
-
-        //salvaModifiche.setOnClickListener();
-        layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        myView = layoutInflater.inflate(R.layout.conversazione, null, false);
-        calendario = view.findViewById(R.id.calendario_agenda);
+        calendario = myView.findViewById(R.id.calendario_agenda);
         calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int anno, int mese, int giorno) {
-                setAnno(anno);
-                setMese(mese);
-                setGiorno(giorno);
-
-
-
-
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                setGiorno(dayOfMonth);
+                setMese(month);
+                setAnno(year);
             }
         });
-        if(getAnno() != 100 && getMese() != 100 && getGiorno() != 100)
-        update(myView);
-        //setOnClick
+
+        if(getGiorno() != 100 && getMese() != 100 && getAnno() != 100)
+            setOnClick(conferma);
     }
 
-    public void update(View myView){
-        butt = myView.findViewById(R.id.salvaAggiunta);
-        setOnClick(butt, myView);
-    }
-
-    private void setOnClick(final Button butt, final View view) {
+    private void setOnClick(final Button butt) {
         butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Bundle bundle = new Bundle();
-                bundle.putInt("annoN", getAnno());
+                bundle.putInt("giornoN", getGiorno());
                 bundle.putInt("meseN", getMese());
-                bundle.putInt("giornoM", getGiorno());
-                bundle.putString("flagAggiunta", "ok");
-                p = new PersonalPageFragment();
-                p.setArguments(bundle);
+                bundle.putInt("annoN", getAnno());
+                profilo = new PersonalPageFragment();
+                profilo.setArguments(bundle);
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 FragmentTransaction transaction = fm.beginTransaction();
-                transaction.replace(R.id.fragment_container, p);
-                transaction.commit();}
+                transaction.replace(R.id.fragment_container, profilo);
+                transaction.commit();
+            }
         });
     }
 
@@ -110,7 +83,4 @@ public class EditAgenda extends Fragment {
     public int getGiorno(){
         return this.giorno;
     }
-
-
-
 }
