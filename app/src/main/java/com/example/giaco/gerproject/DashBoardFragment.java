91b88901bd230a0ea7f3fb0c8005c860b52994c3
EnricoCarnio <enricocarnio@hotmail.it>
2019-03-05 17:03:32 +0100

    Filtro materie funzionante.

--- app/src/main/java/com/example/giaco/gerproject/DashBoardFragment.java
+++ app/src/main/java/com/example/giaco/gerproject/DashBoardFragment.java
@@ -72,15 +72,11 @@ public class DashBoardFragment extends Fragment {
             fs = getArguments().getBoolean("flagSpinner");
             if(getArguments().getString("materiaSelezionata") != null) {
                 materiaSelezionata = getArguments().getString("materiaSelezionata");
-                Toast.makeText(getContext(), "ZZZZZZZZZZZ    " + materiaSelezionata + "    ZZZZZZZZZZZ", Toast.LENGTH_LONG).show();
             }
             else {
                 materiaSelezionata = "DEFAULT";
-                Toast.makeText(getContext(), "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", Toast.LENGTH_LONG).show();
             }
-            //Toast.makeText(getContext(), materiaSelezionata, Toast.LENGTH_SHORT).show();
         }
-        else Toast.makeText(getContext(), "MA CHE DAVVERO? WAWAWAWAWAWAWAWAWAWAWA", Toast.LENGTH_LONG).show();
 
         mparent = view.findViewById(R.id.mparent);
         layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -91,6 +87,7 @@ public class DashBoardFragment extends Fragment {
         ArrayList<String> arrayMaterie = new ArrayList<String>();
         final Context context = ApplicationContextProvider.getContext();
 
+        arrayMaterie.add(" ");
         arrayMaterie.add("DEFAULT");
         arrayMaterie.add("Matematica");
         arrayMaterie.add("Informatica");
@@ -106,7 +103,6 @@ public class DashBoardFragment extends Fragment {
 
         if(materiaSelezionata.equals("DEFAULT")) {
             for (UserTutor t : UserTutorFactory.getInstance().getUserList()) {
-                //Toast.makeText(getContext(), selected, Toast.LENGTH_SHORT).show();
                 switch (t.getMateria()) {
                     case "Fisica":
                         myView = layoutInflater.inflate(R.layout.postitgreen, null, false);
@@ -130,9 +126,7 @@ public class DashBoardFragment extends Fragment {
             }
         }
         else{
-            Toast.makeText(getContext(), "NON SIAMO DEFAULT PUPUPUPUPU", Toast.LENGTH_LONG).show();
             for (UserTutor t : UserTutorFactory.getInstance().getUserList()) {
-                //Toast.makeText(getContext(), selected, Toast.LENGTH_SHORT).show();
                 if(t.getMateria().equals(materiaSelezionata))
                 switch (t.getMateria()) {
                     case "Fisica":
@@ -157,86 +151,11 @@ public class DashBoardFragment extends Fragment {
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-        if(materiaSelezionata.equals("DEFAULT")){
-            Toast.makeText(getContext(), "--------- MAIN ACTIVITY ---------", Toast.LENGTH_LONG).show();
-            for(UserTutor t : UserTutorFactory.getInstance().getUserList()) {
-            //Toast.makeText(getContext(), selected, Toast.LENGTH_SHORT).show();
-                switch (t.getMateria()) {
-                    case "Fisica":
-                        myView = layoutInflater.inflate(R.layout.postitgreen, null, false);
-                        mparent.addView(myView);
-                        b = myView.findViewById(R.id.postitverde);
-                        updatePostit(t, myView);
-                        break;
-                    case "Informatica":
-                        myView = layoutInflater.inflate(R.layout.postitred, null, false);
-                        mparent.addView(myView);
-                        b = myView.findViewById(R.id.postitrosso);
-                        updatePostit(t, myView);
-                        break;
-                    case "Matematica":
-                        myView = layoutInflater.inflate(R.layout.postitblue, null, false);
-                        mparent.addView(myView);
-                        b = myView.findViewById(R.id.postitblu);
-                        updatePostit(t, myView);
-                    break;
-                }
-            }
-        }
-        else {
-            Toast.makeText(getContext(), "--------- NUOVA SELEZIONE---------", Toast.LENGTH_LONG).show();
-
-
-            for (UserTutor t : UserTutorFactory.getInstance().getUserList()) {
-                //Toast.makeText(getContext(), selected, Toast.LENGTH_SHORT).show();
-                if (t.getMateria().equals(selected))
-                    switch (selected) {
-                        case "Fisica":
-                            myView = layoutInflater.inflate(R.layout.postitgreen, null, false);
-                            mparent.addView(myView);
-                            b = myView.findViewById(R.id.postitverde);
-                            updatePostit(t, myView);
-                            break;
-                        case "Informatica":
-                            myView = layoutInflater.inflate(R.layout.postitred, null, false);
-                            mparent.addView(myView);
-                            b = myView.findViewById(R.id.postitrosso);
-                            updatePostit(t, myView);
-                            break;
-                        case "Matematica":
-                            myView = layoutInflater.inflate(R.layout.postitblue, null, false);
-                            mparent.addView(myView);
-                            b = myView.findViewById(R.id.postitblu);
-                            updatePostit(t, myView);
-                            break;
-                    }
-
-            }
-
-        }*/
-
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 String selectedNew = spinner.getSelectedItem().toString();
 
                 if(fs != false ) {
-                    //Toast.makeText(getContext(), "HAI FATTO UN CAMBIAMENTO - " + selectedNew, Toast.LENGTH_SHORT).show();
                     Bundle bundle = new Bundle();
                     bundle.putString("actualUserMail", emailLoggedUser);
                     bundle.putString("materiaSelezionata", selectedNew);
@@ -248,122 +167,12 @@ public class DashBoardFragment extends Fragment {
                     transaction.replace(R.id.fragment_container, dash);
                     transaction.commit();
                 }
-                else {
-                    //Toast.makeText(getContext(), "PRIMA SCELTA - " + selectedNew, Toast.LENGTH_SHORT).show();
+                else
                     fs = true;
-
-                }
-
             }
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
-
-
-
-
     }
-
-        //spinner.getSelectedItem().toString();
-
-
-        /*
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-
-                materiaSelezionata = spinner.getSelectedItem().toString();
-
-                Bundle bundle = new Bundle();
-                bundle.putString("emailTutor", emailLoggedUser);
-                bundle.putString("materiaSelezionata", materiaSelezionata);
-                dash = new DashBoardFragment();
-                dash.setArguments(bundle);
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction = fm.beginTransaction();
-                transaction.replace(R.id.fragment_container, dash);
-                transaction.commit();
-
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-/*
-        ricerca = view.findViewById(R.id.ricercaFiltro);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
-            {
-                Object item = parent.getItemAtPosition(pos);
-                Toast.makeText(context, item.toString(), Toast.LENGTH_SHORT).show();
-                prova = item.toString();
-                if(prova.equals("DEFAULT"))
-                    for(UserTutor t : tutorList) {
-                        switch (t.getMateria()) {
-                            case "Fisica":
-                                myView = layoutInflater.inflate(R.layout.postitgreen, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitverde);
-                                updatePostit(t, myView);
-                                break;
-                            case "Informatica":
-                                myView = layoutInflater.inflate(R.layout.postitred, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitrosso);
-                                updatePostit(t, myView);
-                                break;
-                            case "Matematica":
-                                myView = layoutInflater.inflate(R.layout.postitblue, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitblu);
-                                updatePostit(t, myView);
-                                break;
-                        }
-                    }
-                else{
-                    for(UserTutor t : tutorList) {
-                        if(t.getMateria().equals(prova))
-                        switch (prova) {
-                            case "Fisica":
-                                myView = layoutInflater.inflate(R.layout.postitgreen, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitverde);
-                                updatePostit(t, myView);
-                                break;
-                            case "Informatica":
-                                myView = layoutInflater.inflate(R.layout.postitred, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitrosso);
-                                updatePostit(t, myView);
-                                break;
-                            case "Matematica":
-                                myView = layoutInflater.inflate(R.layout.postitblue, null, false);
-                                mparent.addView(myView);
-                                b = myView.findViewById(R.id.postitblu);
-                                updatePostit(t, myView);
-                                break;
-                        }
-                    }
-                }
-
-            }
-
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-
-            }
-        });
-
-
-
-
-*/
 
     public void updatePostit(UserTutor tutor, View myView) {
 
@@ -431,57 +240,3 @@ public class DashBoardFragment extends Fragment {
         });
     }
 }
-
-/*
-    public void update(String materiaS, View view){
-        ImageView avatar;
-        TextView nomeCognome, materia;
-        ImageView feedback;
-        int feedback_intero;
-        Drawable zerostelle, unastella, duestelle, trestelle,quattrostelle, cinquestelle;
-        zerostelle = getResources().getDrawable(R.drawable.zerostelle);
-        unastella = getResources().getDrawable(R.drawable.unastella);
-        duestelle = getResources().getDrawable(R.drawable.duestelle);
-        trestelle = getResources().getDrawable(R.drawable.trestelle);
-        quattrostelle = getResources().getDrawable(R.drawable.quattrostelle);
-        cinquestelle = getResources().getDrawable(R.drawable.cinquestelle);
-
-        nomeCognome = myView.findViewById(R.id.nomeCognome);
-        nomeCognome.setText("" + tutor.getName() + " " + tutor.getSurname() + "");
-        avatar = myView.findViewById(R.id.avatarTutor);
-        avatar.setImageDrawable(tutor.getImage());
-        feedback = myView.findViewById(R.id.feedback_stelle);
-        materia = myView.findViewById(R.id.materia);
-        materia.setText("" + tutor.getMateria() + "");
-        feedbackList = feedbackFactory.getFeedbackByTutorMail(tutor.getEmail());
-        if(feedbackList.size() > 0) {
-            feedback_intero = feedbackFactory.getVotoTotaleMedio(feedbackList);
-
-            switch (feedback_intero){
-                case 0 :
-                    feedback.setImageDrawable(zerostelle);
-                    break;
-                case 1 :
-                    feedback.setImageDrawable(unastella);
-                    break;
-                case 2 :
-                    feedback.setImageDrawable(duestelle);
-                    break;
-                case 3 :
-                    feedback.setImageDrawable(trestelle);
-                    break;
-                case 4 :
-                    feedback.setImageDrawable(quattrostelle);
-                    break;
-                case 5 :
-                    feedback.setImageDrawable(cinquestelle);
-                    break;
-            }
-        }
-        setOnClick(b,tutor.getEmail(), emailLoggedUser);
-    }
-    }
-
-
-}
-*/
